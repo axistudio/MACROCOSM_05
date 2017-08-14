06f51613cd0d3df67d327789cd6031659d0d0fba
James Yuxi Cao <caoyuxi@me.com>
2017-08-14 17:43:11 -0400

    add mega noise timline
    add mega noise timline

---
+++ Assets/MegaNoise4Timeline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaNoise4Timeline : MonoBehaviour {
+
+    private MegaNoise megaNoise;
+    public Vector3 megaStrengthInTimeline;
+    public float freqInTimeline;
+    public float scaleInTimeline;
+    // Use this for initialization
+    void Start()
+    {
+        megaStrengthInTimeline = new Vector3(0, 0, 0);
+        freqInTimeline = 0.25f;
+        megaNoise = GetComponent<MegaNoise>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        megaNoise.Strength = megaStrengthInTimeline;
+        megaNoise.Scale = 1.0f - scaleInTimeline;
+        megaNoise.Freq = freqInTimeline;
+    }
+}
