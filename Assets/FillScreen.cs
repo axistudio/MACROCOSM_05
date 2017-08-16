75913bc46c78c71153fa7f7dbaafc99db26eb886
James Yuxi Cao <caoyuxi@me.com>
2017-08-16 02:43:00 -0400

    add pro video player
    add pro video player

---
+++ Assets/FillScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillScreen : MonoBehaviour
+{
+    public float movePlane = 0.1f;
+    public Camera cam;
+    void Update()
+    {
+         
+
+            float pos = (cam.nearClipPlane + 0.01f);
+
+            transform.position = cam.transform.position + cam.transform.forward * pos;
+            transform.LookAt(cam.transform);
+            transform.Rotate(90.0f, 0.0f, 0.0f);
+
+            float h = (Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2f) / 10.0f;
+
+            transform.localScale = new Vector3(h * cam.aspect, 1.0f, h);
+
+    }
+}
